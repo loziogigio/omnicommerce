95ea0cfe7eb2f7d47fe6bf7433039b74b91f4d1c
jire87 <y.abia@crowdechain.com>
2023-09-09 17:30:32 +0000

    email and pdf update

--- omnicommerce/controllers/email.py
+++ omnicommerce/controllers/email.py
@@ -1,37 +1,58 @@
 import frappe
 from frappe import _
+from omnicommerce.controllers.pdf import get_pdf_data
+from omnicommerce.controllers.pdf import get_default_letterhead
 
 @frappe.whitelist(allow_guest=True)
-def send_sales_order_confirmation_email(sales_order=None, name=None):
+def send_sales_order_confirmation_email(sales_order=None, name=None , attachment=True , recipients=None):
     # Check if neither sales_order nor name is provided
+    doc_name = "Sales Order"
     if not sales_order and not name:
         return {"status": "Failed", "message": "Missing required parameters."}
 
     # If only name is provided, fetch the Sales Order
     if not sales_order and name:
-        sales_order = frappe.get_doc("Sales Order", name)
+        sales_order = frappe.get_doc(doc_name, name)
 
     # If there's still no sales_order at this point, return failure
     if not sales_order:
         return {"status": "Failed", "message": "Unable to fetch Sales Order."}
 
-    # Fetch email template
-    email_template = frappe.get_doc("Email Template", "sales-order-confirmation")
+    # Check if the specified email template exists
+    if frappe.db.exists("Email Template", "invoice-email-template"):
+        email_template = frappe.get_doc("Email Template", "invoice-email-template")
+    else:
+        default_email_templates = frappe.get_all("Email Template", limit=1)
+        if not default_email_templates:
+            return {"status": "Failed", "message": "No email template found."}
+        email_template = frappe.get_doc("Email Template", default_email_templates[0].name)
+
+
+
+
 
     # Set the email recipients, fetching from the Sales Order customer's details
-    recipients = [sales_order.recipient_email]  # Assuming 'email_id' is the field in Sales Order for customer's email
+    # recipients = [sales_order.recipient_email]  # Assuming 'email_id' is the field in Sales Order for customer's email
+    # Set the email recipients
+    if not recipients:
+        recipients = [sales_order.recipient_email]  # Assuming 'recipient_email' is the field in Sales Order for customer's email
 
-    # Generate PDF of the Sales Order
-    # pdf_data = frappe.attach_print(doctype="Sales Order", name=sales_order.name, file_name=F"sales_order.pdf")
+
+    # Conditionally generate PDF of the Sales Order
+    attachment_data = []
+    if attachment:
+        pdf_data = frappe.attach_print(doctype=doc_name, name=sales_order.name, file_name=F"{sales_order.name}-invoice.pdf" , print_letterhead=True)
+        attachment_data.append(pdf_data)
 
     # Send email
     frappe.sendmail(
         recipients=recipients,
         subject=email_template.subject,
         message=email_template.response,
-        # attachments=[pdf_data],
+        attachments=attachment_data,
         reference_doctype=sales_order.doctype,
         reference_name=sales_order.name
     )
 
     return {"status": "Success", "message": "Email sent successfully."}
+
--- omnicommerce/controllers/pdf.py
+++ omnicommerce/controllers/pdf.py
@@ -1,117 +1,150 @@
-"""
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
-"""
+
 import frappe
-from frappe import _
-from frappe.core.api.file import create_new_folder
-from frappe.model.naming import _format_autoname
-from frappe.realtime import publish_realtime
-from frappe.utils.weasyprint import PrintFormatGenerator
-
-
-def ensure_folder_exists(folder_path):
-    """Ensure that a folder exists. If not, create it."""
-    if not frappe.db.exists("Folder", folder_path):
-        folder_name = folder_path.split("/")[-1]
-        parent_folder = "/".join(folder_path.split("/")[:-1]) or "Home"
-        frappe.get_doc({
-            "doctype": "Folder",
-            "folder_name": folder_name,
-            "is_folder": 1,
-            "parent_folder": parent_folder
-        }).insert()
-
-
-def attach_pdf(doctype, name, title, lang=None, auto_name=None, print_format=None, letterhead=None):
-    """
-    Queue calls this method, when it's ready.
-
-    1. Create necessary folders
-    2. Get raw PDF data
-    3. Save PDF file and attach it to the document
-    """
-
-
-    if lang:
-        frappe.local.lang = lang
-        # unset lang and jenv to load new language
-        frappe.local.lang_full_dict = None
-        frappe.local.jenv = None
-
-
-
-    doctype_folder = "Home/" + doctype
-    title_folder = doctype_folder + "/" + title
-
-    ensure_folder_exists(title_folder)
-    if not frappe.db.exists("Folder", title_folder):
-        frappe.throw(_("Folder {0} does not exist.").format(title_folder))
+from payments.utils.utils import get_payment_gateway_controller
+from mymb_ecommerce.utils.JWTManager import JWTManager, JWT_SECRET_KEY
+jwt_manager = JWTManager(secret_key=JWT_SECRET_KEY)
+from bs4 import BeautifulSoup
+import os
+from frappe.utils.pdf import get_pdf
+from datetime import datetime
+from frappe.utils.file_manager import save_file_on_filesystem
 
 
 
 
-    if frappe.db.get_value("Print Format", print_format, "print_format_builder_beta"):
-        doc = frappe.get_doc(doctype, name)
-        pdf_data = PrintFormatGenerator(print_format, doc, letterhead).render_pdf()
-    else:
-        pdf_data = get_pdf_data(doctype, name, print_format, letterhead)
 
+@frappe.whitelist(allow_guest=True)
+@JWTManager.jwt_required
+def get_sales_order_invoice(order_id):
+    # Fetch the Sales Order using the provided order ID
+    attached_to_doctype = "Sales Order"
 
-
-    return save_and_attach(pdf_data, doctype, name, title_folder, auto_name)
-
-
-
-def create_folder(folder, parent):
-    """Make sure the folder exists and return it's name."""
-    new_folder_name = "/".join([parent, folder])
+    sales_order = frappe.get_doc(attached_to_doctype, order_id)
+    # Return an error message if the Sales Order is not found
+    if not sales_order:
+        return {"error": f"No Sales Order found with ID {order_id}"}
     
-    if not frappe.db.exists("File", new_folder_name):
-        create_new_folder(folder, parent)
-    
-    return new_folder_name
+    # Commented out the user check as you might want to adjust or implement it later
+    # user = frappe.local.jwt_payload['email']
+    # # Verify that the current user is the owner of the Sales Order
+    # if not user == sales_order.customer:
+    #     return {"error": "You do not have permission to access this Sales Order"}
 
+    base_url = frappe.utils.get_url()
+    # Define the desired file path structure
+    file_path_structure = f"Home/Invoices/{sales_order.creation.year}/{sales_order.creation.month}"
 
-def get_pdf_data(doctype, name, print_format: None, letterhead: None):
-    """Document -> HTML -> PDF."""
-    html = frappe.get_print(doctype, name, print_format, letterhead=letterhead)
-    return frappe.utils.pdf.get_pdf(html)
+    # Check if the file already exists
+    existing_file = frappe.db.exists("File", {"file_name": f"{sales_order.name}.pdf", "attached_to_doctype": attached_to_doctype})
+    if existing_file:
+        file_doc = frappe.get_doc("File", existing_file)
+        formatted_date = file_doc.creation.strftime("%d-%m-%Y")
+        return {
+            "name": sales_order.name,
+            "attachment_files": [f"{base_url}{file_doc.file_url}"],
+            "creation_date": formatted_date,
+            "status": sales_order.status,
+            "total": sales_order.total,
+            "items": [{"item_code": item.item_code, "item_name": item.item_name, "qty": item.qty, "rate": item.rate, "image": item.image} for item in sales_order.items]
+        }
 
+    # Generate PDF data
+    pdf_data = get_pdf_data(attached_to_doctype, sales_order.name, print_format="Standard", letterhead=get_default_letterhead())
 
-def save_and_attach(content, to_doctype, to_name, folder, auto_name=None):
+    # Attach PDF to the Sales Order
+    file_doc = save_and_attach(pdf_data, attached_to_doctype, sales_order.name, file_path_structure)
+    formatted_date = file_doc.creation.strftime("%d-%m-%Y")
+
+    # Extract the relevant fields from the Sales Order document and return them as a dictionary
+    return {
+        "name": sales_order.name,
+        "attachment_files": [f"{base_url}{file_doc.file_url}"],
+        "creation_date": formatted_date,
+        "status": sales_order.status,
+        "total": sales_order.total,
+        "items": [{"item_code": item.item_code, "item_name": item.item_name, "qty": item.qty, "rate": item.rate, "image": item.image} for item in sales_order.items]
+    }
+
+def save_and_attach(content, to_doctype, to_name, folder):
     """
     Save content to disk and create a File document.
 
     File document is linked to another document.
     """
-    if auto_name:
-        doc = frappe.get_doc(to_doctype, to_name)
-        # based on type of format used set_name_form_naming_option return result.
-        pdf_name = set_name_from_naming_options(auto_name, doc)
-        file_name = "{pdf_name}.pdf".format(pdf_name=pdf_name.replace("/", "-"))
-    else:
-        file_name = "{to_name}.pdf".format(to_name=to_name.replace("/", "-"))
+    file_name = "{to_name}.pdf".format(to_name=to_name.replace("/", "-"))
+
+    # Check if folder exists, if not create it
+    create_folder_structure(folder)
+
 
     file = frappe.new_doc("File")
     file.file_name = file_name
     file.content = content
     file.folder = folder
-    file.is_private = 0
+    file.is_private = 1
     file.attached_to_doctype = to_doctype
     file.attached_to_name = to_name
     file.save()
+    frappe.db.commit() 
+
     return file
 
 
-def set_name_from_naming_options(autoname, doc):
-    """
-    Get a name based on the autoname field option
-    """
-    _autoname = autoname.lower()
 
-    if _autoname.startswith("format:"):
-        return _format_autoname(autoname, doc)
+def create_folder_structure(folder_path):
+    """Create a folder structure if it doesn't exist."""
+    folders = folder_path.strip("/").split("/")
+    
+    # Ensure the first folder is always "Home", and avoid recreating it.
+    if folders[0] == "Home":
+        folders.pop(0)
+    
+    parent_folder = "Home"
+    
+    for folder in folders:
+        current_folder_path = f"{parent_folder}/{folder}"
+        
+        # Check if current folder exists
+        if not frappe.db.exists("File", {"file_name": folder, "is_folder": 1, "folder": parent_folder}):
+            new_folder = frappe.new_doc("File")
+            new_folder.file_name = folder
+            new_folder.is_folder = 1
+            new_folder.folder = parent_folder
+            new_folder.insert()  # Insert the folder
+            frappe.db.commit() 
+            
+        # Set the parent_folder for next iteration
+        parent_folder = current_folder_path
 
-    return doc.name
+
+
+def get_pdf_data(doctype, name, print_format=None, letterhead=None):
+    """Document -> HTML -> PDF."""
+    # html = frappe.get_print(doctype, name, print_format, letterhead=letterhead)
+
+    # Define the cart as a dictionary
+    cart = {
+        'Samsung Galaxy S20': 10,
+        'iPhone 13': 80
+    }
+
+    html = '<h1>Invoice from Star Electronics e-Store!</h1>'
+
+    # Add items to PDF HTML
+    html += '<ol>'
+    for item, qty in cart.items():
+        html += f'<li>{item} - {qty}</li>'
+    html += '</ol>'
+    
+
+    return get_pdf(html)
+
+
+def get_default_letterhead():
+
+    # Fetch the default Letter Head
+    default_letterhead = frappe.db.get_value('Letter Head', {'is_default': 1}, 'name')
+    if not default_letterhead:
+        frappe.throw(_("Please set a default Letter Head in Letter Head master."))
+    return default_letterhead
