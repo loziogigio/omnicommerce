a5f7e607f7ec29b9d8d8bdc908b8fa1a6096c99c
jire87 <y.abia@crowdechain.com>
2023-09-29 13:54:28 +0000

    email confirm order via html

--- omnicommerce/controllers/email.py
+++ omnicommerce/controllers/email.py
@@ -70,6 +70,63 @@ def send_sales_order_confirmation_email(sales_order=None, name=None , attachment
 
 
 
+@frappe.whitelist(allow_guest=True)
+def send_sales_order_confirmation_email_html(sales_order=None, name=None , attachment=True , recipients=None , email_template="confirm-sales-order-html" , wire_info=""):
+    # Check if neither sales_order nor name is provided
+    doc_name = "Sales Order"
+    if not sales_order and not name:
+        return {"status": "Failed", "message": "Missing required parameters."}
+
+    # If only name is provided, fetch the Sales Order
+    if not sales_order and name:
+        sales_order = frappe.get_doc(doc_name, name)
+
+    # If there's still no sales_order at this point, return failure
+    if not sales_order:
+        return {"status": "Failed", "message": "Unable to fetch Sales Order."}
+
+    # Check if the specified email template exists
+    if frappe.db.exists("Email Template", email_template):
+        email_template = frappe.get_doc("Email Template", email_template)
+    else:
+        default_email_templates = frappe.get_all("Email Template", limit=1)
+        if not default_email_templates:
+            return {"status": "Failed", "message": "No email template found."}
+        email_template = frappe.get_doc("Email Template", default_email_templates[0].name)
+        
+    if not recipients:
+        recipients = [sales_order.recipient_email]   # Assuming 'recipient_email' is the field in Sales Order for customer's email
+
+    
+    context = {
+        **sales_order.as_dict(),
+        "wire_info":wire_info
+        # ... you can add other context variables as needed
+    }
+    try:
+        # Render the email content with the context
+        rendered_email_content = frappe.render_template(email_template.response, context)
+        rendered_subject = frappe.render_template(email_template.subject, context)
+
+        # Send email
+        frappe.sendmail(
+            recipients=recipients,
+            subject=rendered_subject,
+            message=rendered_email_content,
+
+            reference_doctype=sales_order.doctype,
+            reference_name=sales_order.name
+        )
+
+        return {"status": "Success", "message": "Email sent successfully."}
+    except Exception as e:
+        # Log the error
+        frappe.log_error(message=f"Error sending sales order confirmation email for {sales_order.name}: {str(e)}", title=f"Sales Order {sales_order.name} Email Error ")
+
+        # Return a response indicating that there was an error
+        return {"status": "Failed", "message": f"Error encountered: {str(e)}"}
+
+
 from frappe.utils.file_manager import save_file
 
 @frappe.whitelist(allow_guest=True)
